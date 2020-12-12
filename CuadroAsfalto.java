eb960ee1198a78dc791d4decb488b80a5403aa4a
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-12 02:00:43 -0800

    Update CuadroAsfalto.java

--- CuadroAsfalto.java
+++ CuadroAsfalto.java
@@ -5,7 +5,7 @@
 import graficos.Pantalla;
 import graficos.Sprite;
 
-public class CuadroAsfalto extend Cuadro{
+public class CuadroAsfalto extends Cuadro{
   
   public CuadroAsfalto(Sprite sprite){
     super(sprite);
