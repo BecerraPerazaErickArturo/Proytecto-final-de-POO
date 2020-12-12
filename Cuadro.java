e9a03948936c1a5894e29f24c9ecca14a8d54728
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-12 01:39:42 -0800

    Update Cuadro.java

--- Cuadro.java
+++ Cuadro.java
@@ -1,2 +1,25 @@
 //video 16
 //package mapa.cuadro
+
+import graficos.Pantalla;
+import graficos.Sprite;
+
+public abstract class Cuadro{
+  public int x;
+  public int y;
+  
+  public Sprite sprite;
+  
+  public Cuadro(Sprite sprite){
+    this.sprite=sprite;
+  }
+  
+  public void mostrar(int x, int y, Pantalla pantalla){
+  
+  }
+  
+  public boolean solido(){
+    return false;
+  }
+  
+}
