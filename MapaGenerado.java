3192692d3e5727fcd734109d4f76ada85099a1a0
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-12 13:55:42 -0800

    Update MapaGenerado.java

--- MapaGenerado.java
+++ MapaGenerado.java
@@ -3,6 +3,9 @@ public class MapaGenerado extends Mapa{
     super(ancho, largo);
     protected void generarMapa(){
       for(int y =0; y < largo; y++)
+        for(int x = 0; x < ancho; x++){
+          
+        }
     }
   }
 }
