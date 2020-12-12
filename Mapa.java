64a9a3e20e139a6732a0010f91091417cf5f0de2
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-12 15:04:09 -0800

    Update Mapa.java

--- Mapa.java
+++ Mapa.java
@@ -29,10 +29,17 @@ public abstract class Mapa{
   
   }
   
-  public void mostrar(int compensacionX, int compensacionY, Pantalla pantalla){
+  public void mostrar(final int compensacionX, final int compensacionY, final Pantalla pantalla){
     int o = compensacionX >> 5;
     int e = (compensacionX + pantalla.getAncho()) >> 5;
     int n = compensacionY >> 5;
     int s = (compensacionY + pantalla.getLargo()) >> 5;
+  }
+  public Cuadro getCuadro(final int x, final int y){
+    switch(cuadros[x + y * ancho]){
+      case 0: return Cuadro.ASFALTO;
+      case 1: ;
+      default: return null;
+    }
   }
 }
