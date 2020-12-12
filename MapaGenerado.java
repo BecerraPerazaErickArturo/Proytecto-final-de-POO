e4517bbf01b69b17d498567ae4ade78528c3bcf5
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-12 14:15:23 -0800

    Update MapaGenerado.java

--- MapaGenerado.java
+++ MapaGenerado.java
@@ -1,11 +1,15 @@
+import java.util.*;
+
 public class MapaGenerado extends Mapa{
+  private final Random aleatorio = new Random();
   public MapaGenerado(int ancho, int largo){
     super(ancho, largo);
-    protected void generarMapa(){
-      for(int y =0; y < largo; y++)
+   }
+   protected void generarMapa(){
+      for(int y = 0; y < largo; y++){
         for(int x = 0; x < ancho; x++){
-          
+          cuadros[x + y * ancho] = aleatorio.nextInt(3);
         }
+      }
     }
-  }
 }
