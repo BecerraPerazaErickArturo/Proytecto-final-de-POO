4bae4bc22b92c13b7f342ec0c87417041e28d152
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-12 15:12:19 -0800

    Update CuadroVacio.java

--- CuadroVacio.java
+++ CuadroVacio.java
@@ -5,4 +5,7 @@ public class CuadroVacio extends Cuadro{
   public CuadroVacio(Sprite sprite){
     super(sprite);
   }
+  public void mostrar(int x, int y, Pantalla pantalla){
+    pantalla.mostrarCuadro(x, y, this);
+  }
 }
