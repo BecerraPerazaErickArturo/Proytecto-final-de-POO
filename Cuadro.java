ec2e7f150bdb8d54929521cd6bc0f81e53049f6e
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-12 01:51:09 -0800

    Update Cuadro.java

--- Cuadro.java
+++ Cuadro.java
@@ -10,12 +10,15 @@ public abstract class Cuadro{
   
   public Sprite sprite;
   
+  //coleccion de cuadros
+  public static final Cuadro ASFALTO = new CuadroAsfalto();
+  //fin coleccion
+  
   public Cuadro(Sprite sprite){
     this.sprite=sprite;
   }
   
   public void mostrar(int x, int y, Pantalla pantalla){
-  
   }
   
   public boolean solido(){
