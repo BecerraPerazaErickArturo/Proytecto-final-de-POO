838871459b22dfee2cb2c8bec05eec93c92e168f
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-12 15:36:54 -0800

    Update Pantalla.java

--- Pantalla.java
+++ Pantalla.java
@@ -14,28 +14,6 @@ public final class Pantalla{
       pixeles[i] = 0;
     }
   }
-  
-  //temporal
-  public void mostrar(final int compX, final int compY){
-    for(y = 0; y < largo; y++){
-      int posicionY = y + compY;
-      if(posicionY < 0 || posicionY >= largo){
-        continue;
-      }
-      for(int x = 0; x < ancho; x++){
-        int posicionX = x + compX;
-        if(posicionX < 0 || posicionX >= ancho){
-          continue;
-        }
-        
-        //temporal
-        //pixeles[posicionX + posicionY * ancho] = Sprite.nom.pixeles[];
-        pixeles[posicionX + posicionY * ancho] = Sprite.nom.pixeles[(x& MASCARA_SPRITE)+(y&MASCARA_SPRITE) * LADO_SPRITE];
-      }
-    }
-  }
-  //fin temporal
-  
   public void mostrarCuadro(int compensacionX, in compensacionY, Cuadro cuadro){
     for(int y=0; y< cuadro.sprite.getLado(); y++){
       int posicionY= y+compensacionY;
