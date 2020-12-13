36a2a0c88385c0434bf39a832a8b2972aba4c89e
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-12 16:39:02 -0800

    Update Mapa.java

--- Mapa.java
+++ Mapa.java
@@ -42,10 +42,15 @@ public abstract class Mapa{
     }
   }
   public Cuadro getCuadro(final int x, final int y){
+    if(x < 0 || y < 0 || x >= ancho || y >= largo){
+      return Cuadro.VACIO;
+    }
     switch(cuadros[x + y * ancho]){
       case 0: return Cuadro.ASFALTO;
-      case 1: ;
-      default: return cuadro.Vacio;
+      case 1:
+      case 2:
+      case 3:
+      default: return Cuadro.VACIO;
     }
   }
 }
