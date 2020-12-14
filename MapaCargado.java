4019bce40c4fbeb2b6c79e42a1c56f748748105a
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-14 03:15:29 -0800

    Create MapaCargado.java

---
+++ MapaCargado.java
@@ -0,0 +1,15 @@
+//package mapa;
+
+public class MapaCargado extends Mapa{
+
+  private int[] pixeles;
+  
+  public MapaCargado(String ruta){
+    super(ruta);
+  }
+  
+  protected void cargarMapa(String ruta){
+  
+  }
+
+}
