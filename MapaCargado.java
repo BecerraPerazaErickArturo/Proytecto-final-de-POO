5e08a4623fa5f446d497e30521d8ede06867ba57
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-14 03:20:46 -0800

    Update MapaCargado.java

--- MapaCargado.java
+++ MapaCargado.java
@@ -1,5 +1,9 @@
 //package mapa;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class MapaCargado extends Mapa{
 
   private int[] pixeles;
@@ -9,7 +13,9 @@ public class MapaCargado extends Mapa{
   }
   
   protected void cargarMapa(String ruta){
-  
+    BufferedImage imagen = ImageIO.read(MapaCargado.class.getResource(ruta))
+  } catch (IOException e){
+      e.printStackTrace();
   }
 
 }
