b3625bc92f4e6d6cedd1b9c8da30e6f3053fca08
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-14 02:47:50 -0800

    Update Cuadro.java

--- Cuadro.java
+++ Cuadro.java
@@ -13,7 +13,45 @@ public class Cuadro{
   
   //coleccion de cuadros
   public static final Cuadro VACIO = new CuadroVacio(Sprite.VACIO);
-  public static final Cuadro ASFALTO = new CuadroAsfalto(Sprite.ASFALTO);
+  
+  public static final Cuadro CITY_UP_1 = new Cuadro(Sprite.CITY_UP_1); 
+  public static final Cuadro CITY_UP_2 = new Cuadro(Sprite.CITY_UP_2);
+  public static final Cuadro CITY_UP_3 = new Cuadro(Sprite.CITY_UP_3);
+  public static final Cuadro CITY_UP_4 = new Cuadro(Sprite.CITY_UP_4);    
+  public static final Cuadro CITY_UP_5 = new Cuadro(Sprite.CITY_UP_5);  
+  public static final Cuadro MOON = new Cuadro(Sprite.MOON);
+  
+  public static final Cuadro CITY_DOWN_1 = new Cuadro(Sprite.CITY_DOWN_1);  
+  public static final Cuadro CITY_DOWN_2 = new Cuadro(Sprite.CITY_DOWN_2);
+  public static final Cuadro CITY_DOWN_3  = new Cuadro(Sprite.CITY_DOWN_3);
+  public static final Cuadro CITY_DOWN_4  = new Cuadro(Sprite.CITY_DOWN_4);
+  public static final Cuadro CITY_DOWN_5 = new Cuadro(Sprite.CITY_DOWN_5);
+  public static final Cuadro MOON_OBS = new Cuadro(Sprite.MOON_OBS);
+  
+  public static final Cuadro PLANTA_OBS_VOLT_1 = new Cuadro(Sprite.PLANTA_OBS_VOLT_1);
+  public static final Cuadro PLANTA_OBS_VOLT_2  = new Cuadro(Sprite.PLANTA_OBS_VOLT_2);
+  public static final Cuadro PLANTA_OBS_VOLT_3 = new Cuadro(Sprite.PLANTA_OBS_VOLT_3);
+  
+  public static final Cuadro PLANTA_OBS_UP_1 = new Cuadro(Sprite.PLANTA_OBS_UP_1);
+  public static final Cuadro PLANTA_OBS_UP_2 = new Cuadro(Sprite.PLANTA_OBS_UP_2);
+  public static final Cuadro PLANTA_OBS_UP_3 = new Cuadro(Sprite.PLANTA_OBS_UP_3);
+  
+  public static final Cuadro PLANTA_NOR_VOLT_1 = new Cuadro(Sprite.PLANTA_NOR_VOLT_1);
+  public static final Cuadro PLANTA_NOR_VOLT_2 = new Cuadro(Sprite.PLANTA_NOR_VOLT_2);
+  public static final Cuadro PLANTA_NOR_VOLT_3 = new Cuadro(Sprite.PLANTA_NOR_VOLT_3);
+
+  public static final Cuadro SKY_LIGHT_1  = new Cuadro(Sprite.SKY_LIGHT_1);
+  public static final Cuadro SKY_LIGHT_2  = new Cuadro(Sprite.SKY_LIGHT_2);
+  
+  public static final Cuadro SKY_DARK_1 = new Cuadro(Sprite.SKY_DARK_1);
+  public static final Cuadro SKY_DARK_2 = new Cuadro(Sprite.SKY_DARK_2);
+  
+  public static final Cuadro PLANTA_NOR_UP_1 = new Cuadro(Sprite.PLANTA_NOR_UP_1);
+  public static final Cuadro PLANTA_NOR_UP_2 = new Cuadro(Sprite.PLANTA_NOR_UP_2);
+  public static final Cuadro PLANTA_NOR_UP_3  = new Cuadro(Sprite.PLANTA_NOR_UP_3);
+  
+  public static final Cuadro PURPLE = new Cuadro(Sprite.PURPLE);  
+  public static final Cuadro BLUE = new Cuadro(Sprite.BLUE);
   //fin coleccion
   
   public Cuadro(Sprite sprite){
