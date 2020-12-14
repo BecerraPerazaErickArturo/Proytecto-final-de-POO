f311a963ca122242944a72abb36458a13a00253a
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-14 01:46:31 -0800

    Update Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -9,7 +9,27 @@ public final class Sprite{
   
   ///Coleccion de Sprites
   public static final Sprite VACIO = new Sprite(32, 0);
-  public static final Sprite nom = new Sprite(32, 0, 0, HojaSprites.mapa);
+  //                                                    x  y
+  public static final Sprite CITY_UP_1 = new Sprite(32, 0, 0, HojaSprites.mapa); //este se llamaba nom antes
+  public static final Sprite PLANTA_OBS_VOLT_1 = new Sprite(32, 2, 0, HojaSprites.mapa);
+  public static final Sprite MOON_OBS = new Sprite(32, 4, 0, HojaSprites.mapa);
+  public static final Sprite PLANTA_OBS_UP_1 = new Sprite(32, 6, 0, HojaSprites.mapa);
+  
+  public static final Sprite CITY_UP_2 = new Sprite(32, 0, 2, HojaSprites.mapa);
+  public static final Sprite PLANTA_OBS_VOLT_2  = new Sprite(32, 2, 2, HojaSprites.mapa);
+  public static final Sprite CITY_DOWN_1 = new Sprite(32, 4, 2, HojaSprites.mapa);
+  public static final Sprite PLANTA_OBS_UP_2 = new Sprite(32, 6, 2, HojaSprites.mapa);
+  
+  public static final Sprite MOON = new Sprite(32, 0, 4, HojaSprites.mapa);
+  public static final Sprite PLANTA_OBS_VOLT_3 = new Sprite(32, 2, 4, HojaSprites.mapa);
+  public static final Sprite CITY_DOWN_2 = new Sprite(32, 4, 4, HojaSprites.mapa);
+  public static final Sprite PLANTA_OBS_UP_3 = new Sprite(32, 6, 4, HojaSprites.mapa);
+  
+  public static final Sprite CITY_UP_2 = new Sprite(32, 0, 6, HojaSprites.mapa);
+  public static final Sprite PLANTA_OBS_VOLT_2  = new Sprite(32, 2, 6, HojaSprites.mapa);
+  public static final Sprite CITY_DOWN_1 = new Sprite(32, 4, 6, HojaSprites.mapa);
+  public static final Sprite PLANTA_OBS_UP_2 = new Sprite(32, 6, 6, HojaSprites.mapa);
+  
   ///Fin de la coleccion
   
   public Sprite(final int lado, final int columna, final int fila, final HojaSprites hoja){
