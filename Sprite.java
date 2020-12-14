c5af7f4814eed539006aa14f93826a9628165f2f
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-14 02:15:04 -0800

    Update Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -25,11 +25,28 @@ public final class Sprite{
   public static final Sprite CITY_DOWN_2 = new Sprite(32, 4, 4, HojaSprites.mapa);
   public static final Sprite PLANTA_OBS_UP_3 = new Sprite(32, 6, 4, HojaSprites.mapa);
   
-  public static final Sprite CITY_UP_2 = new Sprite(32, 0, 6, HojaSprites.mapa);
-  public static final Sprite PLANTA_OBS_VOLT_2  = new Sprite(32, 2, 6, HojaSprites.mapa);
-  public static final Sprite CITY_DOWN_1 = new Sprite(32, 4, 6, HojaSprites.mapa);
-  public static final Sprite PLANTA_OBS_UP_2 = new Sprite(32, 6, 6, HojaSprites.mapa);
+  public static final Sprite CITY_UP_3 = new Sprite(32, 0, 6, HojaSprites.mapa);
+  public static final Sprite CITY_DOWN_3  = new Sprite(32, 2, 6, HojaSprites.mapa);
+  public static final Sprite BLUE = new Sprite(32, 4, 6, HojaSprites.mapa);
+  public static final Sprite PLANTA_NOR_VOLT_1 = new Sprite(32, 6, 6, HojaSprites.mapa);
   
+  public static final Sprite CITY_UP_4 = new Sprite(32, 0, 8, HojaSprites.mapa);
+  public static final Sprite CITY_DOWN_4  = new Sprite(32, 2, 8, HojaSprites.mapa);
+  public static final Sprite CITY_DOWN_5 = new Sprite(32, 4, 8, HojaSprites.mapa);
+  public static final Sprite PLANTA_NOR_VOLT_2 = new Sprite(32, 6, 8, HojaSprites.mapa);
+    
+  public static final Sprite CITY_UP_5 = new Sprite(32, 0, 10, HojaSprites.mapa);
+  public static final Sprite SKY_LIGHT_1  = new Sprite(32, 2, 10, HojaSprites.mapa);
+  public static final Sprite SKY_DARK_1 = new Sprite(32, 4, 10, HojaSprites.mapa);
+  public static final Sprite PLANTA_NOR_VOLT_3 = new Sprite(32, 6, 10, HojaSprites.mapa);
+  
+  public static final Sprite PLANTA_NOR_UP_1 = new Sprite(32, 0, 12, HojaSprites.mapa);
+  public static final Sprite SKY_LIGHT_2  = new Sprite(32, 2, 12, HojaSprites.mapa);
+  public static final Sprite SKY_DARK_2 = new Sprite(32, 4, 12, HojaSprites.mapa);
+    
+  public static final Sprite PLANTA_NOR_UP_2 = new Sprite(32, 0, 14, HojaSprites.mapa);
+  public static final Sprite PLANTA_NOR_UP_3  = new Sprite(32, 2, 14, HojaSprites.mapa);
+  public static final Sprite PURPLE = new Sprite(32, 4, 14, HojaSprites.mapa);
   ///Fin de la coleccion
   
   public Sprite(final int lado, final int columna, final int fila, final HojaSprites hoja){
