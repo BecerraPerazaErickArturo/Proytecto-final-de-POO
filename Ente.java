faca45d1a48db001f7e85ec271fedaa4b6eee93d
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-14 22:59:36 -0800

    Update Ente.java

--- Ente.java
+++ Ente.java
@@ -1,5 +1,33 @@
 package entes;
 
 public abstract class Ente{
-
+  private int x;
+  private int y;
+  
+  private boolean eliminado = false;
+  
+  protected Mapa mapa;
+  
+  public void actualizar(){
+  }
+  
+  public void mostrar(){
+  }
+  
+  public void eliminar(){
+    eliminado = true;
+  }
+  
+  public int posicionX(){
+    return x;
+  }
+  
+  public int posicionY(){
+    return y;
+  }
+  
+  public boolean estaEliminado(){
+    return eliminado;
+  }
+  
 }
