4a1295d76a3f2e08f085e8794bf7d83375938c4d
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-14 23:07:32 -0800

    Update Criatura.java

--- Criatura.java
+++ Criatura.java
@@ -3,5 +3,34 @@ package entes.criaturas;
 import entes.Ente;
 
 public abstract class Criatura extends Ente{
+  protected Sprite sprite;
+  protected char direccion = 'n'; //norte
+  protected boolean enMovimiento = false;
+  
+  public void actualizar(){
+  }
 
+  public void mostrar(){
+  }
+  
+  public void mover(int desplazamientoX, int desplazamientoY){
+    if(desplazamientoX > 0){
+      direccion ='e'; //este
+    }
+    if(desplazamientoX < 0){
+      direccion ='o'; //oeste
+    }
+    if(desplazamientoY > 0){
+      direccion ='s'; //sur
+    }
+    if(desplazamientoY < 0){
+      direccion ='n'; //norte
+    }
+    if(!estaEliminado()){
+      x +- desplazamientoX;
+    }
+    
+    
+  }
+  
 }
