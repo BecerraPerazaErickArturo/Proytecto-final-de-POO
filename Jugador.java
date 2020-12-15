dba45d45490eaf9a778b3e6b32cd049c0f4f184c
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-14 23:34:18 -0800

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -14,4 +14,21 @@ public class Jugador extends Criatura{
     this.x = posicionX;
     this.y = posicionY;
   }
+  
+  public void actualizar(){
+    int desplazamientoX = 0;
+    int desplazamientoY = 0;
+  
+    if (teclado.arriba) desplazamientoY--; 
+    if (teclado.abajo) desplazamientoY++; 
+    if (teclado.izquierda) desplazamientoX--; 
+    if (teclado.derecha) desplazamientoX++; 
+    if (desplazamientoX !=0 || desplazamientoY !=0 ){
+      mover(desplazamientoX, desplazamientoY);
+    }
+  }
+  
+  public void mostrar(){
+  }
+  
 }
