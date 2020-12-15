8f9198e35707bcb91e2f5792670f77afdecc5a4f
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-15 00:44:01 -0800

    Update and rename Graficos.java to HojaSprites.java

--- HojaSprites.java
+++ HojaSprites.java
@@ -9,6 +9,7 @@ public class HojaSprites{
   
   ///Coleccion de hojas de sprites
   public static HojaSprites mapa = new HojaSprites("///RUTA DE IMAGEN PNG///, 320, 320");
+  public static HojaSprites jugador = new HojaSprites("///RUTA DE IMAGEN jugador PNG///, 320, 320");
   ///Fin de la coleccion
   
   public HojaSprites(final String ruta, final int ancho, final int largo){
