ad48e967e974fe1831df5622aa206b2b1e251dd3
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-14 23:26:58 -0800

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -3,5 +3,15 @@ package entes.criaturas;
 import entes.Ente; 
 
 public class Jugador extends Criatura{
-
+  private Teclado teclado;
+  
+  public Jugador(Teclado teclado){
+    this.teclado = teclado;
+  }
+  
+  public Jugador(Teclado teclado, int posicionX, int posicionY){
+    this.teclado = teclado;
+    this.x = posicionX;
+    this.y = posicionY;
+  }
 }
