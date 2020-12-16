85464d1045139898038872695b906cdeb3ace2fc
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-15 21:37:10 -0800

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -4,7 +4,9 @@ import graficos.Sprites;
 import control.Teclado;
 
 public class Jugador extends Criatura{
+  
   private Teclado teclado;
+  private int aimacion;
   
   public Jugador(Teclado teclado, Sprite sprite){
     this.teclado = teclado;
@@ -22,26 +24,61 @@ public class Jugador extends Criatura{
     int desplazamientoX = 0;
     int desplazamientoY = 0;
   
+    if(animacion < 32767){
+      animacion++;
+    }else {
+      animacion=0;
+    }
+    
     if (teclado.arriba) desplazamientoY--; 
     if (teclado.abajo) desplazamientoY++; 
     if (teclado.izquierda) desplazamientoX--; 
     if (teclado.derecha) desplazamientoX++; 
+    
     if (desplazamientoX !=0 || desplazamientoY !=0 ){
-      
+      mover(desplazamientoX, desplazamientoY);
+      enMovimiento = true;
+    }else{
+      enMovimiento = false;
+    }
+    
       if(direccion == 'n'){
         sprite = Sprite.MAGO_ATRAS_1;
+        if(enMovimiento){
+          if(animacion % 30 > 15){ //esto es la rapidez de la animacion, para mas lento incremente su numero proporcional
+            sprite = Sprite.MAGO_ATRAS_1;
+        }else{
+          sprite = Sprite.MAGO_ATRAS_2;
+        }
       }
       if(direccion == 's'){
         sprite = Sprite.MAGO_FRENTE_1;
+        if(enMovimiento){
+          if(animacion % 30 > 15){ //esto es la rapidez de la animacion, para mas lento incremente su numero proporcional
+            sprite = Sprite.MAGO_FRENTE_1;
+        }else{
+          sprite = Sprite.MAGO_FRENTE_2;
+        }
       }
       if(direccion == 'o'){
         sprite = Sprite.MAGO_IZ_1;
+        if(enMovimiento){
+          if(animacion % 30 > 15){ //esto es la rapidez de la animacion, para mas lento incremente su numero proporcional
+            sprite = Sprite.MAGO_IZ_1;
+        }else{
+          sprite = Sprite.MAGO_IZ_2;
+        }
       }
       if(direccion == 'e'){
-        sprite = Sprite.MAGO_DER_1;
+        sprite = Sprite.MAGO_DER_1;        
+        if(enMovimiento){
+          if(animacion % 30 > 15){ //esto es la rapidez de la animacion, para mas lento incremente su numero proporcional
+            sprite = Sprite.MAGO_DER_1;
+        }else{
+          sprite = Sprite.MAGO_DER_2;
+        
       }
-      mover(desplazamientoX, desplazamientoY);
-    }
+    
   }
   
   public void mostrar(Pantalla pantalla){
