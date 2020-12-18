3608bf50ff4774cc7612fd634db88db8ac98aa2b
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-17 16:12:30 -0800

    Update GestorEstados.java

--- GestorEstados.java
+++ GestorEstados.java
@@ -1,7 +1,42 @@
 //package principal.maquinaestado;
 
+import java.awt.Graphics;
+import principal.maquinaestado.estados.juego.GestorJuego;
+
 public class GestorEstados{
   
-  private
+  private EstadoJuego[] estados;
+  private EstadoJuego estadoActual;
+  
+  public GestorEstados(){
+    iniciarEstados();
+    iniciarEstadoActual();
+    
+  }
+  
+  private void iniciarEstados(){
+    estados = new EstadoJuego[1];
+    estados[0] = new GestorJuego();
+  }
+  
+  private void iniciarEstadoActual(){
+    estadoActual = estados[0];
+  }
 
+  private void actualizar(){
+    estadoActual.actualizar();
+  }
+  
+  private void dibujar(final Graphics g){
+    estadoActual.dibujar(g);
+  }
+  
+  public void cambiarEstadoActual(final int nuevoEstado){
+    estadoActual = estado[nuevoEstado];
+  }
+  
+  public EstadoJuego obtenerEstadoActual(){
+    return estadoActual;
+  }
+  
 }
