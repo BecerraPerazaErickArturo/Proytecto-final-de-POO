85e3fab474ed48ed939362d32fb5f123beba9800
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-17 16:05:09 -0800

    Create GestorJuego.java

---
+++ GestorJuego.java
@@ -0,0 +1,13 @@
+//package principal.maquinaestado.estados.juego;
+
+import java.awt.Graphics;
+
+public class GestorJuego implements EstadoJuego{
+  
+  public void actualizar(){
+  }
+  
+  public void dibujar(Graphics g){
+  
+  }
+}
