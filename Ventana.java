ff3a37380d3847a3d6759f18e34765e6aa235eb4
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-21 14:47:59 -0800

    Update Ventana.java

--- Ventana.java
+++ Ventana.java
@@ -1,4 +1,23 @@
-//package principal.graficos;
+package principal.graficos;
+import javax.swing.JFrame;
 
 public class Ventana extends JFrame{
+  private static final long serialVersionUID = 59794217777239930009L;
+  private String titulo;
+  public Ventana(final String titulo, final SuperficieDibujo sd){
+    this.titulo = titulo;
+    configuracionVentana(sd);
+  }
+  private void configurarVentana(final SuperficiDibujo sd){
+    setTitle(titulo);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setResizable(false);
+    //setIconImage();
+    setLayout(new BorderLlayout());
+    add(sd, BorderLayout.CENTER);
+    //setUndecorated(true);
+    pack();
+    setLocationRelativeTo(null);
+    setVisible(true);
+  }
 }
