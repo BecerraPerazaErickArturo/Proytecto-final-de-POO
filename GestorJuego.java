3756f9ff80a0f3d299cdf15da23c65cf5b8df927
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-21 18:15:16 -0800

    Update GestorJuego.java

--- GestorJuego.java
+++ GestorJuego.java
@@ -4,14 +4,12 @@ import java.awt.Graphics;
 public class GestorJuego implements EstadoJuego{
   
   private GestorMapa gestorMapa;
-  //HojaSprites hs = new  HojaSprites("ruta, 32, true");
   
   public void actualizar(){
     
   }
   
   public void dibujar(Graphics g){
-    //BufferedImage imagen = hs.obtenerSprite(3, 3).getImagen();
-    //g.drawImage(imagen, 100, 100, null);
+    
   }
 }
