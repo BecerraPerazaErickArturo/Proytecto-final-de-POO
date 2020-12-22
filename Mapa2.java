0764db07d563537f15f09fc1f0daf527d9363cfd
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-22 13:22:50 -0800

    Update Mapa2.java

--- Mapa2.java
+++ Mapa2.java
@@ -2,8 +2,16 @@ package principal.mapas;
 omport principal.herramientas.CargadorRecursos;
 
 public class Mapa{
-  String contenido;
+  private int ancho;
+  private int largo;
+  private String[] partes;
   public Mapa(final String ruta){
-    this.contenido = CargadorRecursos.LeerArchivoTexto(ruta);
+    String contenido = CargadorRecursos.LeerArchivoTexto(ruta);
+    partes = contenido.split("*");
+  }
+  public void escribirArray(){
+    for(int i = 0; i < partes.length; i++){
+      System.out.println(partes[i]);
+    }
   }
 }
