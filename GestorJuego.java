4a4598ad56aba3e956f01159d5e2405a4c4514c8
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-21 19:33:27 -0800

    Update GestorJuego.java

--- GestorJuego.java
+++ GestorJuego.java
@@ -2,14 +2,12 @@ package principal.maquinaestado.estados.juego;
 import java.awt.Graphics;
 
 public class GestorJuego implements EstadoJuego{
-  
   private GestorMapa gestorMapa;
-  
+  String texto = CargadorRecursos.LeerArchivoTexto("Aqui va la liga del texto en carpetas");
   public void actualizar(){
-    
   }
-  
   public void dibujar(Graphics g){
-    
+    g.setColor(Color.white);
+    g.drawString(texto, 10, 10);
   }
 }
