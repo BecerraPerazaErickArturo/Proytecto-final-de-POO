c2f09bef9771b44f7e6d55dd2230cbff8238cdaf
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-21 17:20:23 -0800

    Update GestorJuego.java

--- GestorJuego.java
+++ GestorJuego.java
@@ -2,6 +2,9 @@ package principal.maquinaestado.estados.juego;
 import java.awt.Graphics;
 
 public class GestorJuego implements EstadoJuego{
+  
+  private GestorMapa gestorMapa;
+  
   public void actualizar(){
     
   }
