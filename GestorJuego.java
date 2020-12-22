5159d7743160c2bb041ec2c29dc77055de4762f9
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-21 23:41:27 -0800

    Update GestorJuego.java

--- GestorJuego.java
+++ GestorJuego.java
@@ -3,7 +3,8 @@ import java.awt.Graphics;
 
 public class GestorJuego implements EstadoJuego{
   private GestorMapa gestorMapa;
-  String texto = CargadorRecursos.LeerArchivoTexto("Aqui va la liga del texto en carpetas");
+  //String texto = CargadorRecursos.LeerArchivoTexto("Aqui va la liga del texto en carpetas");
+  Mapa mapa = new Mapa("\\Ruta");
   public void actualizar(){
   }
   public void dibujar(Graphics g){
