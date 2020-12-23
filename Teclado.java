e9b3522c0ec3dec39e1a3ac18b4476d05279fdf3
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-23 09:40:26 -0800

    Update Teclado.java

--- Teclado.java
+++ Teclado.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyListener;
 public final class Teclado implements KeyListener{
   
   ///Cambio de nombre a "numeroTeclas" a todo mayusculas pero lo hizo con refactor para que se cambiara por atomatico en cada llamada
-  private final static int numeroTeclas = 256;
+  private final int numeroTeclas = 256;
   private final boolean[] teclas = new boolean[numeroTeclas];
   
   private boolean arriba;
