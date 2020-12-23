3b03a2e9f09d10df79d512997046127008d70c6c
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-22 23:10:03 -0800

    Update Ventana.java

--- Ventana.java
+++ Ventana.java
@@ -4,12 +4,21 @@ import javax.swing.JFrame;
 public class Ventana extends JFrame{
   private static final long serialVersionUID = 59794217777239930009L;
   private String titulo;
+  private final ImageIcon icono;
+  
   public Ventana(final String titulo, final SuperficieDibujo sd){
     this.titulo = titulo;
+    
+    BufferedImage imagen = CargadorRecursos.cargarImagenCompatibleTranslucida("/imagenes/iconos/nombreIcono.png");
+    this.icono = new ImageIcon(imagen);
+    
     configuracionVentana(sd);
+  
   }
+  
   private void configurarVentana(final SuperficiDibujo sd){
     setTitle(titulo);
+    setIconImage(icono.getImage());
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setResizable(false);
     //setIconImage();
