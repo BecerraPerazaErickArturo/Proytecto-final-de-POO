f82275f969fb861bfb5380ebfc512af87f6e7921
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-23 09:58:43 -0800

    Update Constantes.java

--- Constantes.java
+++ Constantes.java
@@ -3,5 +3,6 @@ package principal;
 public class Constantes{
   public static final int LADO_SPRITE = 32;
   public static final int LADO_TILE = 32;
-  
+  public static int ANCHO_PANTALLA;
+  public static int LARGO_PANTALLA;
 }
