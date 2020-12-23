d1a09ce572e87e254bf8e51b27ef8f88a22e01dc
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-23 11:05:22 -0800

    Update GestorJuego.java

--- GestorJuego.java
+++ GestorJuego.java
@@ -8,19 +8,7 @@ public class GestorJuego implements EstadoJuego{
   public void actualizar(){
   }
   public void actualizar(){
-    ///Al restar se vuleve mas lento y al sumar mas rapido en esta serie de If's
-    if(GestorControl.teclado.pulsadoArriba()){
-      jugador.establecerPosicionY(jugador.getPosicionY() - 1);
-    }
-    if(GestorControl.teclado.pulsadoAbajo()){
-      jugador.establecerPosicionY(jugador.getPosicionY() + 1);
-    }
-    if(GestorControl.teclado.pulsadoIzquierda()){
-      jugador.establecerPosicionX(jugador.getPosicionX() - 1);
-    }
-    if(GestorControl.teclado.pulsadoIzquierda()){
-      jugador.establecerPosicionX(jugador.getPosicionX() + 1);
-    }
+
   }
   public void dibujar(Graphics g){
     mapa.dibujar(g, (int)jugador.getPosicionX(), (int)jugador.getPosicionY());
