f3b72d6f84edff4c1298b75c63170d7dcadfec7f
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-22 18:47:07 -0800

    Update GestorJuego.java

--- GestorJuego.java
+++ GestorJuego.java
@@ -5,12 +5,9 @@ public class GestorJuego implements EstadoJuego{
   private GestorMapa gestorMapa;
   //String texto = CargadorRecursos.LeerArchivoTexto("Aqui va la liga del texto en carpetas");
   Mapa mapa = new Mapa("\\Ruta");
+  
   public void actualizar(){
   }
   public void dibujar(Graphics g){
-    g.setColor(Color.white);
-    for(int i = 0; i < mapa.getPaleta().length; i++){
-      g.drawImage(mapa.getSpritePaleta(i).getImagen, i*32, 0, null);
-    }
   }
 }
