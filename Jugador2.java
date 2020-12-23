d74b2544d262b7e6dfcbf4e071cfcbd0d141c3ad
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-23 09:57:29 -0800

    Create Jugador2.java

---
+++ Jugador2.java
@@ -0,0 +1,15 @@
+package principal.entes;
+
+public class Jugador2{
+  private double posicionX;
+  private double posicionY;
+  private Sprite sprite;
+  public Jugador(double posicionX, double posicionY){
+    this.posicionX = posicionX;
+    this.posicionY = posicionY;
+  }
+  public void dibujar(Graphics g){
+    final int 
+    g.setColor(Color.white);
+  }
+}
