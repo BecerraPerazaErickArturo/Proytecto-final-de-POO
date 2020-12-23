84324f72677d1773f99683c2fdf8fd9aaac97f6d
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-23 10:04:51 -0800

    Update Jugador2.java

--- Jugador2.java
+++ Jugador2.java
@@ -9,7 +9,9 @@ public class Jugador2{
     this.posicionY = posicionY;
   }
   public void dibujar(Graphics g){
-    final int 
+    final int centroX = Constantes.ANCHO_PANTALLA / 2 - Constantes.LADO_SPRITE / 2;
+    final int centroY = Constantes.LARGO_PANTALLA / 2 - Constantes.LADO_SPRITE / 2;
     g.setColor(Color.white);
+    g.fillRect(centroX, centroY, Constantes.LADO_SPRITE, Constantes.LADO_SPRITE);
   }
 }
