7b67c19e54e56dd198895579878c7c5aea4508b7
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-23 10:15:58 -0800

    Update GestorJuego.java

--- GestorJuego.java
+++ GestorJuego.java
@@ -8,11 +8,25 @@ public class GestorJuego implements EstadoJuego{
   public void actualizar(){
   }
   public void actualizar(){
-  
+    if(GestorControl.teclado.pulsadoArriba()){
+      jugador.establecerPosicionX(jugador.getPosicionY() - 1);
+    }
+    if(GestorControl.teclado.pulsadoAbajo()){
+      jugador.establecerPosicionX(jugador.getPosicionY() + 1);
+    }
+    if(GestorControl.teclado.pulsadoIzquierda()){
+      jugador.establecerPosicionY(jugador.getPosicionY() - 1);
+    }
+    if(GestorControl.teclado.pulsadoIzquierda()){
+      jugador.establecerPosicionY(jugador.getPosicionY() + 1);
+    }
   }
   public void dibujar(Graphics g){
     mapa.dibujar(g);
     jugador.dibujar(g);
     g.drawImage(logo, 640 - logo.getWidth()- 5, 360 - logo.getHeight() - 5, null);
+    g.setColor(Color.red);
+    g.drawString("X = " + jugador.getPosicionX(), 20, 20);
+    g.drawString("Y = " + jugador.getPosicionY(), 20, 30);
   }
 }
