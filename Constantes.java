e6cbb81361cdb736980dd3a831550822aa151df2
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-26 23:05:17 -0800

    Delete Constantes.java

--- Constantes.java
+++
@@ -1,9 +0,0 @@
-package principal;
-
-public class Constantes{
-  public static final int LADO_SPRITE = 32;
-  public static final int LADO_TILE = 32;
-  public static int ANCHO_PANTALLA;
-  public static int LARGO_PANTALLA;
-  public static int APS;
-}
