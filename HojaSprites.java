ef4fbf6fa265505299b6cb0fe5257e64c2d354bf
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-27 11:48:35 -0800

    Update HojaSprites.java

--- HojaSprites.java
+++ HojaSprites.java
@@ -6,8 +6,12 @@ public class HojaSprites{
 	private final int ancho;
 	private final int alto;
 	public final int[] pixeles;
+
+	// Rutas de hojas de sprites
 	public static HojaSprites BaseGraficos = new HojaSprites("C:\\Users\\hp1\\Desktop\\1.png", 320, 320);
 	public static HojaSprites jugador = new HojaSprites("C:\\Users\\hp1\\Desktop\\2.png", 320, 320);
+	//--------------------------
+	
 	public HojaSprites(final String ruta, final int ancho, final int alto){
 		this.ancho = ancho;
 		this.alto = alto;
