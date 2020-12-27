32e2132867c527151042ea56084ab5486463e87a
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-26 23:34:57 -0800

    Update HojaSprites.java

--- HojaSprites.java
+++ HojaSprites.java
@@ -8,18 +8,17 @@ public class HojaSprites{
 	public final int[] pixeles;
 	public static HojaSprites desierto = new HojaSprites("/texturas/desierto.png", 320, 320);
 	public static HojaSprites jugador = new HojaSprites("/texturas/jugador.png", 320, 320);
-	public HojaSprites(final String ruta, final int ancho, final int alto) {
+	public HojaSprites(final String ruta, final int ancho, final int alto){
 		this.ancho = ancho;
 		this.alto = alto;
 		pixeles = new int[ancho * alto];
 		BufferedImage imagen;
 		try{
-			imagen = ImageIO.read(HojaSprites.class.getResource(ruta))
+			imagen = ImageIO.read(HojaSprites.class.getResource(ruta));
 			imagen.getRGB(0, 0, ancho, alto, pixeles, 0, ancho);
 		}catch(IOException e){
 			e.printStackTrace();
 		}
-
 	}
 	public int obtenAncho(){
 		return ancho;
