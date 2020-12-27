76900c2bc114a3de070107c57a3a4b077f2ed065
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-26 23:59:46 -0800

    Update Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -1,31 +1,21 @@
-public final class Sprite {
+public final class Sprite{
 	private final int lado;
-
 	private int x;
 	private int y;
-
 	public int[] pixeles;
 	private HojaSprites hoja;
-
-	// Colección de sprites del personaje
 	public static final Sprite ABAJO0 = new Sprite(32, 0, 0, 0, HojaSprites.jugador);
 	public static final Sprite ABAJO1 = new Sprite(32, 0, 1, 0, HojaSprites.jugador);
 	public static final Sprite ABAJO2 = new Sprite(32, 0, 2, 0, HojaSprites.jugador);
-
 	public static final Sprite ARRIBA0 = new Sprite(32, 1, 0, 0, HojaSprites.jugador);
 	public static final Sprite ARRIBA1 = new Sprite(32, 1, 1, 0, HojaSprites.jugador);
 	public static final Sprite ARRIBA2 = new Sprite(32, 1, 2, 0, HojaSprites.jugador);
-
 	public static final Sprite DERECHA0 = new Sprite(32, 2, 0, 0, HojaSprites.jugador);
 	public static final Sprite DERECHA1 = new Sprite(32, 2, 1, 0, HojaSprites.jugador);
 	public static final Sprite DERECHA2 = new Sprite(32, 2, 2, 0, HojaSprites.jugador);
-
 	public static final Sprite IZQUIERDA0 = new Sprite(32, 3, 0, 0, HojaSprites.jugador);
 	public static final Sprite IZQUIERDA1 = new Sprite(32, 3, 1, 0, HojaSprites.jugador);
 	public static final Sprite IZQUIERDA2 = new Sprite(32, 3, 2, 0, HojaSprites.jugador);
-	// Fin de la colección
-
-	// Colección de sprites del mapa desértico
 	public static final Sprite VACIO = new Sprite(32, 0);
 	public static final Sprite ASFALTO = new Sprite(32, 0, 0, 0, HojaSprites.desierto);
 	public static final Sprite ARENA = new Sprite(32, 1, 0, 0, HojaSprites.desierto);
@@ -50,53 +40,41 @@ public final class Sprite {
 	public static final Sprite PUERTA_INFERIOR = new Sprite(32, 8, 1, 0, HojaSprites.desierto);
 	public static final Sprite OXIDO = new Sprite(32, 9, 0, 0, HojaSprites.desierto);
 	public static final Sprite PUERTA_SUPERIOR_CENTRAL = new Sprite(32, 8, 0, 0, HojaSprites.desierto);
-	// Fin de la colección
-
-	public Sprite(final int lado, final int columna, final int fila, final int version, final HojaSprites hoja) {
+	public Sprite(final int lado, final int columna, final int fila, final int version, final HojaSprites hoja){
 		this.lado = lado;
-
 		pixeles = new int[lado * lado];
-
 		this.x = columna * lado;
 		this.y = fila * lado;
 		this.hoja = hoja;
-
 		cargarSprite(version);
 	}
-
-	public Sprite(final int lado, final int color) {
+	public Sprite(final int lado, final int color){
 		this.lado = lado;
 		pixeles = new int[lado * lado];
-
-		for (int i = 0; i < pixeles.length; i++) {
+		for (int i = 0; i < pixeles.length; i++){
 			pixeles[i] = color;
 		}
 	}
-
-	public int obtenLado() {
+	public int obtenLado(){
 		return lado;
 	}
-
-	private void cargarSprite(int version) {
-		if (version == 0) {
+	private void cargarSprite(int version){
+		if (version == 0){
 			cargaNormal();
-		} else {
+		}else{
 			cargaManipulada(version);
 		}
 	}
-
-	private void cargaNormal() {
-		for (int y = 0; y < lado; y++) {
-			for (int x = 0; x < lado; x++) {
+	private void cargaNormal(){
+		for (int y = 0; y < lado; y++){
+			for (int x = 0; x < lado; x++){
 				pixeles[x + y * lado] = hoja.pixeles[(x + this.x) + (y + this.y) * hoja.obtenAncho()];
 			}
 		}
 	}
-
-	private void cargaManipulada(int version) {
+	private void cargaManipulada(int version){
 		int[] pixelesTemporales = iniciarPixelesTemporales();
-
-		switch (version) {
+		switch (version){
 			case 1 :
 				invertirX(pixelesTemporales);
 				break;
@@ -122,59 +100,48 @@ public final class Sprite {
 				cargaNormal();
 		}
 	}
-
-	private int[] iniciarPixelesTemporales() {
+	private int[] iniciarPixelesTemporales(){
 		int[] pixelesTemporales = new int[lado * lado];
-		for (int y = 0; y < lado; y++) {
-			for (int x = 0; x < lado; x++) {
+		for (int y = 0; y < lado; y++){
+			for (int x = 0; x < lado; x++){
 				pixelesTemporales[x + y * lado] = hoja.pixeles[(x + this.x) + (y + this.y) * hoja.obtenAncho()];
 			}
 		}
 		return pixelesTemporales;
 	}
-
-	// 1
-	private void invertirX(int[] pixelesTemporales) {
+	private void invertirX(int[] pixelesTemporales){
 		int i = 0;
-		for (int y = 0; y < lado; y++) {
-			for (int x = lado - 1; x > -1; x--) {
+		for (int y = 0; y < lado; y++){
+			for (int x = lado - 1; x > -1; x--){
 				pixeles[i] = pixelesTemporales[x + y * lado];
 				i++;
 			}
 		}
 	}
-
-	// 2
-	private void invertirY(int[] pixelesTemporales) {
+	private void invertirY(int[] pixelesTemporales){
 		int i = 0;
-		for (int y = lado - 1; y > -1; y--) {
-			for (int x = 0; x < lado; x++) {
+		for (int y = lado - 1; y > -1; y--){
+			for (int x = 0; x < lado; x++){
 				pixeles[i] = pixelesTemporales[x + y * lado];
 				i++;
 			}
 		}
 	}
-
-	// 3
-	private void invertirXY(int[] pixelesTemporales) {
-		for (int i = 0; i < pixeles.length; i++) {
+	private void invertirXY(int[] pixelesTemporales){
+		for (int i = 0; i < pixeles.length; i++){
 			pixeles[i] = pixelesTemporales[pixelesTemporales.length - 1 - i];
 		}
 	}
-
-	// 4
-	private void rotar90I(int[] pixelesTemporales) {
+	private void rotar90I(int[] pixelesTemporales){
 		int i = 0;
-		for (int x = lado - 1; x > -1; x--) {
-			for (int y = 0; y < lado; y++) {
+		for (int x = lado - 1; x > -1; x--){
+			for (int y = 0; y < lado; y++){
 				pixeles[i] = pixelesTemporales[x + y * lado];
 				i++;
 			}
 		}
 	}
-
-	// 5
-	private void rotar90D(int[] pixelesTemporales) {
+	private void rotar90D(int[] pixelesTemporales){
 		int i = 0;
 		for (int x = 0; x < lado; x++) {
 			for (int y = lado - 1; y > -1; y--) {
@@ -183,23 +150,19 @@ public final class Sprite {
 			}
 		}
 	}
-
-	// 6
-	private void rotarI90InvertirY(int[] pixelesTemporales) {
+	private void rotarI90InvertirY(int[] pixelesTemporales){
 		int i = 0;
-		for (int x = 0; x < lado; x++) {
-			for (int y = 0; y < lado; y++) {
+		for (int x = 0; x < lado; x++){
+			for (int y = 0; y < lado; y++){
 				pixeles[i] = pixelesTemporales[x + y * lado];
 				i++;
 			}
 		}
 	}
-
-	// 7
-	private void rotarD90InvertirY(int[] pixelesTemporales) {
+	private void rotarD90InvertirY(int[] pixelesTemporales){
 		int i = 0;
-		for (int x = lado - 1; x > -1; x--) {
-			for (int y = lado - 1; y > -1; y--) {
+		for (int x = lado - 1; x > -1; x--){
+			for (int y = lado - 1; y > -1; y--){
 				pixeles[i] = pixelesTemporales[x + y * lado];
 				i++;
 			}
