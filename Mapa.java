db0135f5e5a9ffd8932fb0915355e43c3b17a179
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-27 00:24:30 -0800

    Update Mapa.java

--- Mapa.java
+++ Mapa.java
@@ -45,107 +45,40 @@ public abstract class Mapa{
 	    return Cuadro.VACIO;
 	}
 	switch (cuadros[x + y * ancho]){
-	case 0:
-		return Cuadro.SKY_1;
-		break;
-	case 1:
-		return Cuadro.SKY_2;
-		break;
-	case 2:
-		return Cuadro.SKY_3;
-		break;
-	case 3:
-		return Cuadro.SKY_4;
-		break;
-	case 4:
-		return Cuadro.CITY_1;
-		break;
-	case 5:
-		return Cuadro.CITY_2;
-		break;
-	case 6:
-		return Cuadro.CITY_3;
-		break;
-	case 7:
-		return Cuadro.CITY_4;
-		break;
-	case 8:
-		return Cuadro.CITY_5;
-		break;
-	case 9:
-		return Cuadro.CIRCLE_SKY_1;
-		break;
-	case 10:
-		return Cuadro.CIRCLE_SKY_2;
-		break;
-	case 11:
-		return Cuadro.CIRCLE_SKY_3;
-		break;
-	case 12:
-		return Cuadro.CIRCLE_SKY_4;
-		break;
-	case 13: 
-		return SKY;
-		break;
-	case 14: 
-		return PURPLE;
-		break;
-	case 15: 
-		return ESQUINA_DARK_1;
-		break;
-	case 16: 
-		return ESQUINA_DARK_2;
-		break;
-	case 17: 
-		return ESQUINA_DARK_3;
-		break;
-	case 18: 
-		return ESQUINA_LIGHT_1;
-		break;
-	case 19: 
-		return ESQUINA_LIGHT_2;
-		break;
-	case 20: 
-		return ESQUINA_LIGHT_3;
-		break;
-	case 21: 
-		return ESQUINA_LIGHT_MOR_1;
-		break;
-	case 22: 
-		return ESQUINA_LIGHT_MOR_2;
-		break;
-	case 23: 
-		return ESQUINA_LIGHT_MOR_3;
-		break;
-	case 24: 
-		return ESQUINA_DARK_AZ_1;
-		break;
-	case 25: 
-		return ESQUINA_DARK_AZ_2;
-		break;
-	case 26: 
-		return ESQUINA_DARK_AZ_3;
-		break;
-	case 27: 
-		return LADO_DARK_1;
-		break;
-	case 28: 
-		return LADO_DARK_2;
-		break;
-	case 29: 
-		return LADO_LIGHT;
-		break;
-	case 30: 
-		return LADO_LIGHT_MOR_1;
-		break;
-	case 31: 
-		return LADO_LIGHT_MOR_2;
-		break;
-	case 32: 
-		return LADO_DARK_AZ;
-		break;
-	default:
-	    return Cuadro.VACIO;
+	case 0: return Cuadro.SKY_1;
+	case 1: return Cuadro.SKY_2;
+	case 2: return Cuadro.SKY_3;
+	case 3: return Cuadro.SKY_4;
+	case 4: return Cuadro.CITY_1;
+	case 5: return Cuadro.CITY_2;
+	case 6: return Cuadro.CITY_3
+	case 7: return Cuadro.CITY_4;
+	case 8: return Cuadro.CITY_5;
+	case 9: return Cuadro.CIRCLE_SKY_1;
+	case 10: return Cuadro.CIRCLE_SKY_2;
+	case 11: return Cuadro.CIRCLE_SKY_3;
+	case 12: return Cuadro.CIRCLE_SKY_4;
+	case 13: return SKY;
+	case 14: return PURPLE;
+	case 15: return ESQUINA_DARK_1;
+	case 16: return ESQUINA_DARK_2;
+	case 17: return ESQUINA_DARK_3;
+	case 18: return ESQUINA_LIGHT_1;
+	case 19: return ESQUINA_LIGHT_2;
+	case 20: return ESQUINA_LIGHT_3;
+	case 21: return ESQUINA_LIGHT_MOR_1;
+	case 22: return ESQUINA_LIGHT_MOR_2;
+	case 23: return ESQUINA_LIGHT_MOR_3;
+	case 24: return ESQUINA_DARK_AZ_1;
+	case 25: return ESQUINA_DARK_AZ_2;
+	case 26: return ESQUINA_DARK_AZ_3;
+	case 27: return LADO_DARK_1;
+	case 28: return LADO_DARK_2;
+	case 29: return LADO_LIGHT;
+	case 30: return LADO_LIGHT_MOR_1;
+	case 31: return LADO_LIGHT_MOR_2;
+	case 32: return LADO_DARK_AZ;
+	default: return Cuadro.VACIO;
 	}
     }
 }
