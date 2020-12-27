66b1078c43539085918e085dbee4fdc10aaf4cb8
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-27 00:21:46 -0800

    Update MapaGenerado.java

--- MapaGenerado.java
+++ MapaGenerado.java
@@ -8,7 +8,7 @@ public class MapaGenerado extends Mapa{
     protected void generarMapa(){
 	for (int y = 0; y < alto; y++){
 	    for (int x = 0; x < ancho; x++){
-		cuadros[x + y * ancho] = aleatorio.nextInt(13);
+		cuadros[x + y * ancho] = aleatorio.nextInt(33);
 	    }
 	}
     }
