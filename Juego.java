ce5a77282c51eb7cd715cb9031fbde8fc87e7963
BecerraPerazaErickArturo <72172184+BecerraPerazaErickArturo@users.noreply.github.com>
2020-12-27 23:46:11 -0800

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -31,16 +31,16 @@ public class Juego extends Canvas implements Runnable{
 	private static BufferedImage imagen = new BufferedImage(ANCHO, ALTO, BufferedImage.TYPE_INT_RGB);
 	private static int[] pixeles = ((DataBufferInt) imagen.getRaster().getDataBuffer()).getData();
 	
-	private File icono = new File("C:\\Users\\hp1\\Desktop\\Proyecto-main\\icono.png");
+	private File icono = new File("C:/Users/hp1/Desktop/Proyecto-main/icono.png");
 	//haber si funciona ;---;
-	//private static final ImageIcon icono = new ImageIcon(Juego.class.getResource("C:\\Users\\hp1\\Desktop\\Proyecto-main\\icono.png"));
+	//private static final ImageIcon icono = new ImageIcon(Juego.class.getResource("C:/Users/hp1/Desktop/Proyecto-main/icono.png"));
 	
 	private Juego(){
 		setPreferredSize(new Dimension(ANCHO, ALTO));
 		pantalla = new Pantalla(ANCHO, ALTO);
 		teclado = new Teclado();
 		addKeyListener(teclado);
-		mapa = new MapaCargado("C:\\Users\\hp1\\Desktop\\Proyecto-main\\mapa.png");
+		mapa = new MapaCargado("C:/Users/hp1/Desktop/Proyecto-main/mapa.png");
 		jugador = new Jugador(teclado, Sprite.MAGO_FRENTE_1, 225, 225);
 		ventana = new JFrame(NOMBRE);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
