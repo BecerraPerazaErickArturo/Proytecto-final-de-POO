38374701ec11add5482c0c3d10d63960c468759a
ValeriaSanchezPerez <72220141+ValeriaSanchezPerez@users.noreply.github.com>
2020-12-27 23:38:46 -0800

    Update Pantalla.java

--- Pantalla.java
+++ Pantalla.java
@@ -4,61 +4,70 @@ public final class Pantalla{
 	private int diferenciaX;
 	private int diferenciaY;
 	public final int[] pixeles;
-	public Pantalla(final int ancho, final int alto){
+
+	public Pantalla(final int ancho, final int alto) {
 		this.ancho = ancho;
 		this.alto = alto;
 		pixeles = new int[ancho * alto];
 	}
-	public void limpiar(){
-		for (int i = 0; i < pixeles.length; i++){
+
+	public void limpiar() {
+		for (int i = 0; i < pixeles.length; i++) {
 			pixeles[i] = 0;
 		}
 	}
-	public void mostrarCuadro(int compensacionX, int compensacionY, Cuadro cuadro){
+
+	public void mostrarCuadro(int compensacionX, int compensacionY, Cuadro cuadro) {
 		compensacionX -= diferenciaX;
 		compensacionY -= diferenciaY;
-		for(int y = 0; y < cuadro.sprite.obtenLado(); y++){
+		for (int y = 0; y < cuadro.sprite.obtenLado(); y++) {
 			int posicionY = y + compensacionY;
-			for(int x = 0; x < cuadro.sprite.obtenLado(); x++){
+			for (int x = 0; x < cuadro.sprite.obtenLado(); x++) {
 				int posicionX = x + compensacionX;
-				if(posicionX < -cuadro.sprite.obtenLado() || posicionX >= ancho || posicionY < 0 || posicionY >= alto){
+				if (posicionX < -cuadro.sprite.obtenLado() || posicionX >= ancho || posicionY < 0
+						|| posicionY >= alto) {
 					break;
 				}
-				if(posicionX < 0){
+				if (posicionX < 0) {
 					posicionX = 0;
 				}
 				pixeles[posicionX + posicionY * ancho] = cuadro.sprite.pixeles[x + y * cuadro.sprite.obtenLado()];
 			}
 		}
 	}
-	public void mostrarJugador(int compensacionX, int compensacionY, Jugador jugador){
+
+	public void mostrarJugador(int compensacionX, int compensacionY, Jugador jugador) {
 		compensacionX -= diferenciaX;
 		compensacionY -= diferenciaY;
-		for(int y = 0; y < jugador.obtenSprite().obtenLado(); y++){
+		for (int y = 0; y < jugador.getSprite().obtenLado(); y++) {
 			int posicionY = y + compensacionY;
-			for(int x = 0; x < jugador.obtenSprite().obtenLado(); x++){
+			for (int x = 0; x < jugador.getSprite().obtenLado(); x++) {
 				int posicionX = x + compensacionX;
-				if(posicionX < -jugador.obtenSprite().obtenLado() || posicionX >= ancho || posicionY < 0 || posicionY >= alto){
+				if (posicionX < -jugador.getSprite().obtenLado() || posicionX >= ancho || posicionY < 0
+						|| posicionY >= alto) {
 					break;
 				}
-				if(posicionX < 0){
+				if (posicionX < 0) {
 					posicionX = 0;
 				}
-				int colorPixelJugador = jugador.obtenSprite().pixeles[x + y * jugador.obtenSprite().obtenLado()];
-				if(colorPixelJugador != 0xff02dad8){ //el color del fondo del personaje
+				int colorPixelJugador = jugador.getSprite().pixeles[x + y * jugador.getSprite().obtenLado()];
+				if (colorPixelJugador != 0xff02dad8) { // el color del fondo del personaje
 					pixeles[posicionX + posicionY * ancho] = colorPixelJugador;
 				}
 			}
 		}
 	}
-	public void estableceDiferencia(final int diferenciaX, final int diferenciaY){
+
+	public void estableceDiferencia(final int diferenciaX, final int diferenciaY) {
 		this.diferenciaX = diferenciaX;
 		this.diferenciaY = diferenciaY;
 	}
-	public int obtenAncho(){
+
+	public int obtenAncho() {
 		return ancho;
 	}
-	public int obtenAlto(){
+
+	public int obtenAlto() {
 		return alto;
 	}
 }
